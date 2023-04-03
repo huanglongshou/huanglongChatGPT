609f36c2b3b7e43ca88a129d12c32e6ab37596a8
woshicuo <63390090+huanglongshou@users.noreply.github.com>
2023-04-03 23:56:22 +0800

    Update ChuanhuChatbot.py

--- ChuanhuChatbot.py
+++ ChuanhuChatbot.py
@@ -384,7 +384,7 @@ with gr.Blocks(css=customCSS, theme=small_and_beautiful_theme) as demo:
 
 logging.info(
     colorama.Back.GREEN
-    + "\n川虎的温馨提示：访问 http://localhost:7860 查看界面"
+    + "\nAir的温馨提示：访问 http://localhost:7860 查看界面"
     + colorama.Style.RESET_ALL
 )
 # 默认开启本地服务器，默认可以直接从IP访问，默认不创建公开分享链接
